34125e1032ef24ecf737db1814b8b9fb90e06ada
李志明 <lizhiming@minivision.cn>
2018-09-26 14:43:42 +0800

    pic list

--- src/main.js
+++ src/main.js
@@ -7,12 +7,15 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+// require styles
+import 'swiper/dist/css/swiper.css'
+
 import '@/styles/index.scss' // global css
 
 import App from './App'
 import router from './router'
 import store from './store'
-
 import i18n from './lang' // Internationalization
 import './icons' // icon
 import './errorLog' // error log
@@ -20,6 +23,8 @@ import './permission' // permission control
 import './mock' // simulation data
 
 import * as filters from './filters' // global filters
+
+Vue.use(VueAwesomeSwiper)
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
--- src/views/picture/list/index.vue
+++ src/views/picture/list/index.vue
@@ -1,12 +1,47 @@
 <template>
-  <div>
-    view
+  <div  class="components-container">
+     <div class="filter-container">
+      <el-input :placeholder="$t('table.title')" v-model="listQuery.title" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter"/>
+      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search">{{ $t('table.search') }}</el-button>
+    </div>
+     <swiper :options="swiperOption">
+      <swiper-slide><img src="https://raw.githubusercontent.com/wiki/clawhub/pic-bed/test/LOGO_gaitubao_com_568x568.png"></swiper-slide>
+      <swiper-slide><img src="https://raw.githubusercontent.com/wiki/clawhub/pic-bed/test/LOGO_gaitubao_com_568x568.png"></swiper-slide>
+        <swiper-slide><img src="https://raw.githubusercontent.com/wiki/clawhub/pic-bed/test/LOGO_gaitubao_com_568x568.png"></swiper-slide>
+      <swiper-slide><img src="https://raw.githubusercontent.com/wiki/clawhub/pic-bed/test/LOGO_gaitubao_com_568x568.png"></swiper-slide>
+        <swiper-slide><img src="https://raw.githubusercontent.com/wiki/clawhub/pic-bed/test/LOGO_gaitubao_com_568x568.png"></swiper-slide>
+      <swiper-slide><img src="https://raw.githubusercontent.com/wiki/clawhub/pic-bed/test/LOGO_gaitubao_com_568x568.png"></swiper-slide>
+        <swiper-slide><img src="https://raw.githubusercontent.com/wiki/clawhub/pic-bed/test/LOGO_gaitubao_com_568x568.png"></swiper-slide>
+      <swiper-slide><img src="https://raw.githubusercontent.com/wiki/clawhub/pic-bed/test/LOGO_gaitubao_com_568x568.png"></swiper-slide>
+        <swiper-slide><img src="https://raw.githubusercontent.com/wiki/clawhub/pic-bed/test/LOGO_gaitubao_com_568x568.png"></swiper-slide>
+      <swiper-slide><img src="https://raw.githubusercontent.com/wiki/clawhub/pic-bed/test/LOGO_gaitubao_com_568x568.png"></swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
 <script>
-
+import waves from '@/directive/waves' // 水波纹指令
 export default {
-  name: 'View'
+  name: 'View',
+  directives: {
+    waves
+  },
+  data () {
+    return {
+      listQuery: {
+        title: undefined
+      },
+      swiperOption: {
+        slidesPerView: 3,
+        slidesPerColumn: 2,
+        spaceBetween: 30,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        }
+      }
+    }
+  }
 }
 </script>
